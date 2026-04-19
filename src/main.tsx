358608d18b1cb4f1e97e3a088591228d3217678d
Son Bram van der Burg <son@vdburg.site>
2026-04-19 14:12:59 +0200

    Revert "Feat/desktop and window component"

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import './i18n/i18n'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './pages/Desktop.tsx'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
